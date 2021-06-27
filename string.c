de4f0d873ed2c69ccf0300edc543e1b325fb75fe
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-06-27 21:41:19 +0800

    Create string.c

---
+++ string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h> // strlen
+
+#define pr printf
+
+int main()
+{
+  // STR is defined as an ARR of CHARs
+  // DIFF between CHAR ARR and STR is the trailing '\0'
+
+  char s[] = "geeks";     // '\0' is implied
+  pr("%ld\n", sizeof(s)); // 6
+  pr("%ld\n", strlen(s)); // 5
+  puts(s);                // geeks
+
+  // ZERO BYTE as STR terminator
+  // bin 0000 0000
+  // dec 0
+  // hex 00
+
+  // below are the same
+  s[3] = 0;    // NUM 0 is ASCII for NULL
+  s[3] = '\0'; // notice the SINGLE quote
+
+  pr("%ld\n", sizeof(s)); // 6
+  pr("%ld\n", strlen(s)); // 3
+  puts(s);                // gee
+}
