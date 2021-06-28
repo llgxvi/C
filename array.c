e6d2ccf22b72e66ae7eddbdbdb12a0e34c959021
llgxvi <58665866+llgxvi@users.noreply.github.com>
2021-06-28 19:00:57 +0800

    Update array.c

--- array.c
+++ array.c
@@ -10,24 +10,23 @@ void main()
   pr("%p\n", &a);
 
   // warning: format ‘%d’ expects argument of
-  // type ‘int’, but argument 2 has type ‘char *’
+  // type ‘int’, but argument 2 has type ‘char *’ ⬅️
   // a points to the 1st ele
-  // a + 1 results in addr increment of 4
+  // a + 1 results in addr increment of 1
   // pr("%d", a);
 
   // warning: format ‘%d’ expects argument of
-  // type ‘int’, but argument 2 has type ‘char (*)[6]’
+  // type ‘int’, but argument 2 has type ‘char (*)[6]’ ⬅️
   // &a points to the whole arr
-  // &a + 1 results in addr increment of 4 * 6
+  // &a + 1 results in addr increment of 1 * 6
   // pr("%d", &a);
 
   // warning: comparison of distinct pointer types
   // lacks a cast
   // pr("%d\n", a==&a);
 
-  pr("%d\n", a==&a[0]); // 1
-
-  // pr("%d\n", (char*)a==a); // 1
+  pr("%d\n", a==&a[0]);    // 1
+  pr("%d\n", (char*)a==a); // 1
 
   // warning: format ‘%d’ expects argument of
   // type ‘int’, but argument 2 has type ‘long int’
